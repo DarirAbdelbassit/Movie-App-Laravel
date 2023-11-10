0fe2cbf4f7a7925cbe6bbd2bccd0a214e34d9049
DarirAbdelbassit <abdelbassitdarir@gmail.com>
2023-11-10 01:11:27 +0100

    Refactor movie views

--- resources/views/pages/movies/index.blade.php
+++ resources/views/pages/movies/index.blade.php
@@ -1,19 +1,19 @@
 @extends('layouts.app')
 @section('content')
-    <div class="container mx-auto px-4 pt-16">
+<div class="container mx-auto px-4 pt-16">
         <!-- start pouplar-movies -->
-        <x-movies-container title="Popular Movies">
+        <x-elements-container title="Popular Movies">
             @foreach ($popularMovies as $movie)
                 <x-movie-card :movie="$movie" />
             @endforeach
-        </x-movies-container>
+        </x-elements-container>
         <!-- end pouplar-movies -->
         <!-- start playing-movies -->
-        <x-movies-container title="Playing Movies">
+        <x-elements-container title="Playing Movies">
             @foreach ($nowPlayingMovies as $movie)
                 <x-movie-card :movie="$movie" />
             @endforeach
-        </x-movies-container>
+        </x-elements-container>
         <!-- end now-playing-movies -->
     </div>
 @endsection
--- resources/views/pages/movies/show.blade.php
+++ resources/views/pages/movies/show.blade.php
@@ -37,19 +37,45 @@
                         @endforeach
                     </div>
                 </div>
-                @if ($movie['videos'])
-                    <div class="mt-12">
-                        <a href="https://www.youtube.com/watch?v={{ $movie['videos'] }}" target="_blank"
-                            class="flex inline-flex items-center bg-orange-500 text-gray-900 rounded font-semibold px-5 py-4 hover:bg-orange-600 transition ease-in-out duration-150">
-                            <svg class="w-6 fill-current" viewBox="0 0 24 24">
-                                <path d="M0 0h24v24H0z" fill="none" />
-                                <path
-                                    d="M10 16.5l6-4.5-6-4.5v9zM12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z" />
-                            </svg>
-                            <span class="ml-2">Play Trailer</span>
-                        </a>
+                    <div x-data="{ isOpen: false }" x-cloak>
+                        @if ($movie['videos'])
+                            <div class="mt-12">
+                                <button
+                                @click="isOpen = true"
+                                    class="flex inline-flex items-center bg-orange-500 text-gray-900 rounded font-semibold px-5 py-4 hover:bg-orange-600 transition ease-in-out duration-150">
+                                    <svg class="w-6 fill-current" viewBox="0 0 24 24">
+                                        <path d="M0 0h24v24H0z" fill="none" />
+                                        <path
+                                            d="M10 16.5l6-4.5-6-4.5v9zM12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z" />
+                                    </svg>
+                                    <span class="ml-2" >Play Trailer</span>
+                                </button>
+                            </div>
+                        @endif
+                        <!-- Start trailer modal-->
+                        <div style="background-color: rgba(0, 0, 0, .5);"
+                            class="fixed top-0 left-0 w-full h-full flex items-center shadow-lg overflow-y-auto"
+                            x-show="isOpen">
+                            <div class="container mx-auto  lg:px-32 rounded-lg overflow-y-auto  ">
+                                <div class="bg-gray-900 rounded m-14" @click.outside="isOpen = false">
+                                    <div class="flex justify-end pr-4 pt-2">
+                                        <button
+                                        @click="isOpen = false"
+                                            class="text-3xl leading-none hover:text-gray-300">&times;
+                                        </button>
+                                    </div>
+                                    <div class="modal-body px-8 py-8">
+                                        <div class="responsive-container overflow-hidden relative" style="padding-top: 56.25%">
+                                            <iframe class="responsive-iframe absolute top-0 left-0 w-full h-full"
+                                                src="https://www.youtube.com/embed/{{ $movie['videos'] }}" style="border:0;"
+                                                allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Start trailer modal-->
                     </div>
-                @endif
             </div>
         </div>
         <!-- End movie-info -->
